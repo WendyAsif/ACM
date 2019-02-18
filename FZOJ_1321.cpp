da1ff7c0479af4a9a80507a3e8bdd950f540028c
kingfxb <45414799+kingfxb@users.noreply.github.com>
2019-02-18 18:55:16 +0800

    Update FZOJ_1321.cpp

--- FZOJ_1321.cpp
+++ FZOJ_1321.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 int n,m;
+int v[30],c[30];
+
+int dp[30000+4000];
+
 int main(){
+  scanf("%d %d",&n,&m);
+  
+  for(int i=1;i<=m;i++){
+    scanf("%d %d",&c[i],&v[i]);
+    v[i]*=c[i];
+  }
+  
+  for(int i=1;i<=m;i++){
+    for(int j=n;j>=c[i];j--){
+      dp[j]=max(dp[j],dp[j-c[i]]+v[i]);
+    }
+  }
+  
+  printf("%d",dp[n]);
 
   return 0;
 }
